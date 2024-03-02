0b834d9c6d04081c551050d200c0b94d276e5566
Dori7o <ricardo.dori7o@gmail.com>
2024-03-02 22:14:52 +0000

    added event driven msg

--- project/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
+++ project/accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -17,4 +17,7 @@ public class Accounts extends BaseEntity {
     private String accountType;
 
     private String branchAddress;
+
+    @Column(name = "communication_sw")
+    private boolean communicationSw;
 }
